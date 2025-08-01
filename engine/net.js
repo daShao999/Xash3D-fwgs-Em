d1f529fe125f608dbfee00a279f715a859955334
yohimik <yohimik@gmail.com>
2025-08-01 23:25:26 +0400

    fix: net

--- engine/net.js
+++ engine/net.js
@@ -1,8 +1,8 @@
 mergeInto(LibraryManager.library, {
     sendto_js: function(sockfd, packets, sizes, packet_count, seq_num, to, to_len) {
-        console.log('sendto');
+        return moduleArg.sendto?.(sockfd, packets, sizes, packet_count, seq_num, to, to_len) ?? 0
     },
     recvfrom_js: function(sockfd, buf, len, flags, src_addr, addrlen) {
-        console.log('recvfrom');
+        return moduleArg.recvfrom?.(sockfd, buf, len, flags, src_addr, addrlen) ?? -1
     },
 });
